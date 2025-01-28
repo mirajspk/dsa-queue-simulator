d552e3829fb020191d5e76cc76ec1f9cc05f0dfc
Miraj <147530194+mirajspk@users.noreply.github.com>
2025-01-28 23:56:40 +0545

    setup sfml and cmake

---
+++ Makefile
@@ -0,0 +1,9 @@
+all:
+	@echo "Configuring..."
+	@cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+	@echo "Building..."
+	@cmake --build build
+
+clean:
+	@echo "Cleaning up..."
+	@rm -rf build
---
+++ src/simulator.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Window.hpp>
+
+int main()
+{
+    sf::Window window(sf::VideoMode({800, 600}), "simulator");
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+    }
+}

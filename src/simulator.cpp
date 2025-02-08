955e3038ad6621f8867df2f70aa15841b1a4120b
Miraj <147530194+mirajspk@users.noreply.github.com>
2025-02-08 16:39:48 +0545

    implement enqueue & dequeue for vehicle on lane D2

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include "header/VehicleClass.hpp"
 #include "header/LaneTrigger.hpp"
+#include "header/VehicleQueue.hpp"
 #include <iostream>
 
 int main()
@@ -15,8 +16,11 @@ int main()
     }
     sf::Sprite sprite(texture);
     sf::Clock clock;
+
+    VehicleQueue queue_D2;
     Vehicle vehicle(1, Lane::D2, Lane::C1, Route::MOVE_D_TO_C);
     LaneTrigger laneTrigger(sf::Vector2f(0.0f, 480.0f), sf::Vector2f(450.0f, 30.0f), Lane::D2);
+    bool flag = false;
 
     while (window.isOpen())
     {   sf::Event event;
@@ -28,10 +32,16 @@ int main()
         float deltaTime = clock.restart().asSeconds();
         vehicle.update(deltaTime);
 
-        if (laneTrigger.isVehicleOnLane(vehicle)) {
-            std::cout << "Vehicle is on lane " << static_cast<int>(laneTrigger.getLane()) << std::endl;
-        }else{
-            std::cout<<"Vehicle is not on lane"<<std::endl;
+        if (laneTrigger.isVehicleOnLane(vehicle) && !flag) {
+            std::cout << "Enqueue " <<vehicle.getID()<<" to "<< static_cast<int>(laneTrigger.getLane()) << std::endl;
+            queue_D2.enqueue(vehicle);
+            flag = true;
+        } else if(!laneTrigger.isVehicleOnLane(vehicle)) {
+            if(flag){
+                queue_D2.dequeue();
+                std::cout << "Dequeue " <<vehicle.getID()<<" from "<< static_cast<int>(laneTrigger.getLane()) << std::endl;
+                flag=false;
+            }
         }
 
         window.clear();
